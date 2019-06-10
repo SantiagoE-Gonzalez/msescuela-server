0ce5e179bfe4804a49604d31d2e82a029fef7807
Santiago Gonzalez <sangonzalez.94@gmail.com>
2019-06-10 17:27:36 -0300

    Se agrega el cross-origin

--- src/main/java/ar/com/santiagogonzalez/ejemplos/msescuelaserver/MsescuelaServerApplication.java
+++ src/main/java/ar/com/santiagogonzalez/ejemplos/msescuelaserver/MsescuelaServerApplication.java
@@ -3,13 +3,37 @@ package ar.com.santiagogonzalez.ejemplos.msescuelaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableEurekaServer
+@CrossOrigin(origins = "http://localhost:80")
 public class MsescuelaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsescuelaServerApplication.class, args);
 	}
 
+	
+	@Bean
+	public CorsFilter corsFilter() {
+	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    final CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowCredentials(true);
+	    config.addAllowedOrigin("*");
+	    config.addAllowedHeader("*");
+	    config.addAllowedMethod("OPTIONS");
+	    config.addAllowedMethod("HEAD");
+	    config.addAllowedMethod("GET");
+	    config.addAllowedMethod("PUT");
+	    config.addAllowedMethod("POST");
+	    config.addAllowedMethod("DELETE");
+	    config.addAllowedMethod("PATCH");
+	    source.registerCorsConfiguration("/**", config);
+	    return new CorsFilter(source);
+	}
 }
